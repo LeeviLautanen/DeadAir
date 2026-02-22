55e1dd7d4a3fc50955b7b32f11273cd310ea592d
Leevi Lautanen <leevi.lautanen02@gmail.com>
2026-02-22 19:30:24 +0200

    Improved logger

--- Assets/Scripts/Logger.cs
+++ Assets/Scripts/Logger.cs
@@ -1,26 +1,36 @@
+public enum LogLevel
+{
+    Log,
+    Warning,
+    Error
+}
+
 public class Logger
 {
-    public bool Enabled { get; set; } = true;
+    private readonly bool enabled;
+    private readonly LogLevel level;
 
-    public Logger(bool enabled = true)
+    public Logger(bool enabled = true, LogLevel level = LogLevel.Log)
     {
-        Enabled = enabled;
+        this.enabled = enabled;
+        this.level = level;
     }
 
     public void Log(string message)
     {
-        if (!Enabled) return;
+        if (!enabled) return;
         UnityEngine.Debug.Log(message);
     }
 
     public void Warning(string message)
     {
-        if (!Enabled) return;
+        if (!enabled || level < LogLevel.Warning) return;
         UnityEngine.Debug.LogWarning(message);
     }
 
     public void Error(string message)
     {
+        if (!enabled || level < LogLevel.Error) return;
         UnityEngine.Debug.LogError(message);
     }
 }
