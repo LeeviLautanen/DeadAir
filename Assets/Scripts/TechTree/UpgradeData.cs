421f2a3f0175e25c4b2ae2b86ca80525e74c4cc2
Leevi Lautanen <leevi.lautanen02@gmail.com>
2026-03-21 23:19:19 +0200

    Converted display name to generate dynamically based off text

--- Assets/Scripts/TechTree/UpgradeData.cs
+++ Assets/Scripts/TechTree/UpgradeData.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Game/UpgradeData")]
 public class UpgradeData : ScriptableObject
 {
-    public string DisplayName;
-    public string Description;
+    public string DisplayNameTemplate;
+    public string DisplayName => InsertValuesIntoTemplate(DisplayNameTemplate);
+    public string DescriptionTemplate;
+    public string Description => InsertValuesIntoTemplate(DescriptionTemplate);
     public int ResearchCost;
     public List<UpgradeModifier> Modifiers = new();
+
+    public string InsertValuesIntoTemplate(string template)
+    {
+        if (string.IsNullOrEmpty(template))
+            return "No name";
+
+        foreach (var m in Modifiers)
+        {
+            string key = "{" + m.ModType + "}";
+            string valueText = m.IsPercent ? $"{m.Value}%" : $"{m.Value}";
+            template = template.Replace(key, valueText);
+        }
+
+        return template;
+    }
 }
